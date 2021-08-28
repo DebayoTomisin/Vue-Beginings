69d4fd5a8bb3f3b206efff29f0d74c7d07792bb6
Debayo Tomisin <debayotomisin07@gmail.com>
2021-08-27 23:48:40 -0700

    getting into it

---
+++ src/components/Counter.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>This is the first line of vue code i'll be writing</div>
+    <div id="counter">Counter: {{ counter }}</div>
+</template>
+
+<script>
+export default {
+    name: 'Counter'
+}
+</script>
